2fa82012eadf2634ec227bcf618d75c9ee73105a
Alex Ostapenko <alex.alx13@gmail.com>
2020-05-28 21:29:54 +0200

    proper parameters

--- nr-tracing/main.go
+++ nr-tracing/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"time"
@@ -19,8 +20,16 @@ import (
 )
 
 type config struct {
-	Port           int    `env:"PORT" envDefault:"8080"`
-	NewRelicAPIKey string `env:"NEW_RELIC_API_KEY,required"`
+	Port             int     `env:"PORT" envDefault:"8080"`
+	NewRelicAPIKey   string  `env:"NEW_RELIC_API_KEY,required"`
+	NewRelicTraceURL url.URL `env:"NEW_RELIC_TRACE_URL,required"`
+}
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: false,
+		FullTimestamp: false,
+	})
 }
 
 func copyHeader(dst, src http.Header) {
@@ -60,7 +69,7 @@ func getNewRelicHandler(cfg config) func(http.ResponseWriter, *http.Request) {
 
 		req, err := http.NewRequest(
 			"POST",
-			"https://trace-api.newrelic.com/trace/v1",
+			cfg.NewRelicTraceURL.String(),
 			bytes.NewBuffer(payload),
 		)
 
@@ -71,7 +80,7 @@ func getNewRelicHandler(cfg config) func(http.ResponseWriter, *http.Request) {
 		}
 
 		copyHeader(req.Header, r.Header)
-		req.Header.Set("Host", "trace-api.newrelic.com")
+		req.Header.Set("Host", cfg.NewRelicTraceURL.Host)
 		req.Header.Set("Api-Key", cfg.NewRelicAPIKey)
 		req.Header.Set("Data-Format", "zipkin")
 		req.Header.Set("Data-Format-Version", "2")
